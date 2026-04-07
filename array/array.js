0257eee2d3eb5967b2d8e1ef9638d92310f0b7b8
happlejoseph <vyshnavanil18753@gamil.com>
2026-04-07 15:59:35 +0530

    array

---
+++ array/array.js
@@ -0,0 +1,50 @@
+
+// //for of loop
+// let n = [1,2,3,66,79]
+// let d = null
+// for (let item of n){
+//     if(item%3==0)
+//         d = item
+// }
+// console.log(d);
+
+// //find
+// let dividableNumber = n.find(x=>x%3==0)
+// console.log(dividableNumber);
+
+// let dividableNumberIndex = n.findIndex(x=>x%3==0)
+// console.log(dividableNumberIndex);
+
+// let dividableNumberLastIndex = n.findLastIndex(x=>x%2!=0);
+// console.log(dividableNumberLastIndex);
+
+// let isSomeDivisible = n.some(x=>x%3==0);
+// console.log(isSomeDivisible);
+
+// let everyNUmber = n.every(x=>x>10);
+// console.log(everyNUmber);
+
+// //for each
+// n.forEach((item,index,arr)=>{
+//     console.log(item*9);
+    
+// });
+
+// let squareNumber = n.map(x=>x*x)
+// console.log(squareNumber);
+
+// //filter
+// let oddNumber = n.filter(x=>x%2!=0)
+// console.log(oddNumber);
+
+// //reduce
+// let sum = n.reduce((accumulator,item)=>{
+//     return accumulator+item
+// },0);
+// console.log(sum);
+
+let n = [12,5,8,130,44,9,20,15];
+d = null
+for (let item of n){
+    if ()
+}
