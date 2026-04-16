f77df329e45016ebffb85f3ba30d93d120ed4466
happle joseph <happlejoseph66@gmail.com>
2026-04-17 00:03:54 +0530

    crud change to array object

--- curd-1.js
+++ curd-1.js
@@ -1,34 +1,42 @@
 
 let names = []; //we need a place to store names.
 
-function addName (){                        //Create a function to add a name.
-    let name = prompt('Enter the name');    //Ask user for a name.
-    names.push(name);                       //Store it in the array.
+function addNameAndCourse (){                 //Create a function to add a name.
+    let name = prompt('Enter the name');      //Ask user for a name.
+    let course = prompt('Enter the course');  //Ask user for a course.
+    names.push({name, course});               //Store it in the array.
 }
 
-function getNames (){    //its is the function to get the names from the array and return them as a string to display. 
-    let str = '';        //Create an empty string to store the names before displaying them..
+function getNamesAndCourses (){              //its is the function to get the names from the array and return them as a string to display. 
+    let str = '';                           //Create an empty string to store the names before displaying them..
     names.forEach((item, index)=> {         //Loop through the array and add each name to the string with its index.
-        str += `${index + 1}. ${item}\n`;
+        str += `${index + 1}) ${item.name} - ${item.course}\n`;
     });
     return str;
 }
 
-function displayNames (){   //Create a function to display names.
-    let str = getNames();
+function displayNamesAndCourses (){   //Create a function to display names and courses.
+    let str = getNamesAndCourses();
     alert(str);
 }
 
 function editName (){
-    let str = getNames();
-    let index = Number(prompt('Enter the number to edit:'));  //Ask which name to edit
+    let str = getNamesAndCourses();
+    let index = Number(prompt(`${str}Enter the number to edit:`));  //Ask which name to edit
     let newName = prompt('Enter the new name:'); //Ask for the new name.
-    names[index - 1] = newName; //Update the name in the array.
+    names[index - 1].name = newName; //Update the name in the array.
+}
+
+function editCourse (){
+    let str = getNamesAndCourses();
+    let index = Number(prompt(`${str}Enter the number to edit:`)); //Ask which courese to edit.
+    let newCourse = prompt('Enter the new course:');
+    names[index - 1].course = newCourse
 }
 
 function deleteName (){
-    let str = getNames();
-    let index = Number(prompt('Enter the number to delete:')); //Ask which name to delete.
+    let str = getNamesAndCourses();
+    let index = Number(prompt(`${str}Enter the number to delete:`)); //Ask which name to delete.
     names.splice(index - 1, 1);  // splice(start, deleteCount) //Remove the name from the array.
 }
 
